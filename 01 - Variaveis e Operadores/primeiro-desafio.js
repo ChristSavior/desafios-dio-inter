9765afc38455a8f5a6537d81c110f8212624e757
Rodrigo Guilherme <rodrigog.a@hotmail.com>
2022-10-28 21:13:09 -0300

    Atualização de desafios

---
+++ 01 - Variaveis e Operadores/primeiro-desafio.js
@@ -0,0 +1,21 @@
+/* Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas:
+
+1 - Preço do combustível;
+2 - Gasto médio do carro por KM;
+3 - Distância em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar esta viagem.
+*/
+
+const precoCombustivel = 5.79;
+const KmPorLitro = 10;
+const distanciaKm = 100;
+
+const litrosGastos = distanciaKm / KmPorLitro;
+
+//Resultado
+const gastoTotal = litrosGastos * precoCombustivel;
+
+console.log(gastoTotal.toFixed(2));
