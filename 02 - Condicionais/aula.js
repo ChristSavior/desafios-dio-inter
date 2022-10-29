591ea034cf8aef00f6cfbd76d00fdc7b05d752ea
Rodrigo Guilherme <rodrigog.a@hotmail.com>
2022-10-29 13:16:32 -0300

    Nova organização

---
+++ 02 - Condicionais/aula.js
@@ -0,0 +1,14 @@
+
+/* Booleans - Condicionais que são apenas verdadeiro ou falso*/
+
+
+const camisetaRodrigoAzul = true;
+const camisetaRodrigoVerde = false;
+
+const numero = 10;
+
+
+
+const numeroPar = numero % 2;
+
+console.log(numeroPar);
