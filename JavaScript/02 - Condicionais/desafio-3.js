bea3557dc662f5219624b944adff30c6bc0e2f00
Rodrigo Guilherme <rodrigog.a@hotmail.com>
2022-10-29 20:50:22 -0300

    Desafios 1, 2, e 3

---
+++ JavaScript/02 - Condicionais/desafio-3.js
@@ -0,0 +1,36 @@
+/* O IMC - Índice de Massa Corporal é um critério da OMS para dar uma indicação sobre a condição de peso de uma pessoa adulta;
+
+Formula do IMC:
+IMC = peso / (altura * altura);
+
+Elabore um algoritmo que dado o peso e a altura de um adulto, mostre sua condição de acordo com a tabela abaixo:
+
+IMC em adultos:
+    - Abaixo de 18.5 Abaixo do peso
+    - Entre 18.5 e 25 Peso normal
+    - Entre 25 e 30 Acima do peso
+    - Entre 30 e 40 Obesidade;
+    - Acima de 40 Obesidade Grave;
+*/
+
+
+const peso = 65;
+const altura = 1.70;
+const imc = peso  / (altura * altura);
+//const imc = peso / Math.pow(altura, 2);
+
+console.log(imc.toFixed(2));
+
+if (imc < 18.5) {
+    console.log('Abaixo do peso');
+} else if (imc >= 18.5 && imc < 25) {
+    console.log('Peso Normal');
+} else if (imc >= 25 && imc < 30) {
+    console.log('Acima do peso');
+} else if (imc >= 30 && imc < 40) {
+    console.log('Obesidade');
+} else {
+    console.log('Obesidade Grave');
+}
+
+
---
+++ JavaScript/02 - Condicionais/desafio.js
@@ -0,0 +1,27 @@
+/* Faça um programa para calcular o valor de uma viagem.
+
+Você terá 5 variáveis. Sendo elas;
+    1 - Preço do etanol;
+    2 - Preço da gasolina;
+    3 - O tipo de combustível que está no seu carro;
+    4 - Gasto médio de combustível do carro por KM;
+    5 - Distância em KM da viagem;
+    
+    * Imprima no console o valor que será gasto para realizar esta viagem;
+*/
+
+const precoEtanol = 5.79;
+const precoGasolina = 6.66;
+const kmPorLitroEtanol = 10;
+const kmPorLitroGasolina = 16;
+const distanciaViagem = 200;
+const tipoCombustivel = 'Gasolina';
+
+const gastoEtanol = distanciaViagem / kmPorLitroEtanol * precoEtanol;
+const gastoGasolina = distanciaViagem / kmPorLitroGasolina * precoGasolina;
+
+if (tipoCombustivel === 'Etanol') {
+    console.log(gastoEtanol.toFixed(2));
+} else if (tipoCombustivel === 'Gasolina') {
+    console.log(gastoGasolina.toFixed(2));
+}
